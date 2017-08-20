6579649a479a2ba3a2fefbc1bae1238c0685cc2d
jmiller <hoppercodes@gmail.com>
2017-08-20 01:03:47 -0700

    Refactor Field Names and remove edit button

--- app/src/main/java/com/hoppercodes/devoter/MainActivity.java
+++ app/src/main/java/com/hoppercodes/devoter/MainActivity.java
@@ -1,13 +1,11 @@
 package com.hoppercodes.devoter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -26,7 +24,7 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity implements OnClickListener
 {
-    EditText editRollno,editName,editMarks;
+    EditText editAuthorName, editAuthorEmailAddress, editAuthorEmailMessage;
     Button btnAdd,btnDelete,btnModify,btnView,btnViewAll,btnShowInfo;
     SQLiteDatabase db;
     /** Called when the activity is first created. */
@@ -35,101 +33,79 @@ public class MainActivity extends Activity implements OnClickListener
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editRollno=(EditText)findViewById(R.id.editRollno);
-        editName=(EditText)findViewById(R.id.editName);
-        editMarks=(EditText)findViewById(R.id.editMarks);
+        editAuthorName =(EditText)findViewById(R.id.editAuthorName);
+        editAuthorEmailAddress =(EditText)findViewById(R.id.editAuthorEmailAddress);
+        editAuthorEmailMessage =(EditText)findViewById(R.id.editAuthorEmailMessage);
         btnAdd=(Button)findViewById(R.id.btnAdd);
         btnDelete=(Button)findViewById(R.id.btnDelete);
-        btnModify=(Button)findViewById(R.id.btnModify);
         btnView=(Button)findViewById(R.id.btnView);
         btnViewAll=(Button)findViewById(R.id.btnViewAll);
         btnShowInfo=(Button)findViewById(R.id.btnShowInfo);
         btnAdd.setOnClickListener(this);
         btnDelete.setOnClickListener(this);
-        btnModify.setOnClickListener(this);
         btnView.setOnClickListener(this);
         btnViewAll.setOnClickListener(this);
         btnShowInfo.setOnClickListener(this);
-        db=openOrCreateDatabase("StudentDB", Context.MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS student(rollno VARCHAR,name VARCHAR,marks VARCHAR);");
+        db=openOrCreateDatabase("DevoterDB", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS delayEmail(auName VARCHAR,auEmail VARCHAR,auMessage VARCHAR);");
     }
     public void onClick(View view)
     {
         if(view==btnAdd)
         {
-            if(editRollno.getText().toString().trim().length()==0||
-                    editName.getText().toString().trim().length()==0||
-                    editMarks.getText().toString().trim().length()==0)
+            if(editAuthorName.getText().toString().trim().length()==0||
+                    editAuthorEmailAddress.getText().toString().trim().length()==0||
+                    editAuthorEmailMessage.getText().toString().trim().length()==0)
             {
                 showMessage("Error", "Please enter all values");
                 return;
             }
-            db.execSQL("INSERT INTO student VALUES('"+editRollno.getText()+"','"+editName.getText()+
-                    "','"+editMarks.getText()+"');");
+            db.execSQL("INSERT INTO delayEmail VALUES('"+ editAuthorName.getText()+"','"+ editAuthorEmailAddress.getText()+
+                    "','"+ editAuthorEmailMessage.getText()+"');");
             showMessage("Success", "Record added");
             clearText();
         }
         if(view==btnDelete)
         {
-            if(editRollno.getText().toString().trim().length()==0)
+            if(editAuthorName.getText().toString().trim().length()==0)
             {
-                showMessage("Error", "Please enter Rollno");
+                showMessage("Error", "Please enter auName");
                 return;
             }
-            Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+            Cursor c=db.rawQuery("SELECT * FROM delayEmail WHERE auName='"+ editAuthorName.getText()+"'", null);
             if(c.moveToFirst())
             {
-                db.execSQL("DELETE FROM student WHERE rollno='"+editRollno.getText()+"'");
+                db.execSQL("DELETE FROM delayEmail WHERE auName='"+ editAuthorName.getText()+"'");
                 showMessage("Success", "Record Deleted");
             }
             else
             {
-                showMessage("Error", "Invalid Rollno");
-            }
-            clearText();
-        }
-        if(view==btnModify)
-        {
-            if(editRollno.getText().toString().trim().length()==0)
-            {
-                showMessage("Error", "Please enter Rollno");
-                return;
-            }
-            Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
-            if(c.moveToFirst())
-            {
-                db.execSQL("UPDATE student SET name='"+editName.getText()+"',marks='"+editMarks.getText()+
-                        "' WHERE rollno='"+editRollno.getText()+"'");
-                showMessage("Success", "Record Modified");
-            }
-            else
-            {
-                showMessage("Error", "Invalid Rollno");
+                showMessage("Error", "Invalid auName");
             }
             clearText();
         }
         if(view==btnView)
         {
-            if(editRollno.getText().toString().trim().length()==0)
+            if(editAuthorName.getText().toString().trim().length()==0)
             {
-                showMessage("Error", "Please enter Rollno");
+                showMessage("Error", "Please enter auName");
                 return;
             }
-            Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+            Cursor c=db.rawQuery("SELECT * FROM delayEmail WHERE auName='"+ editAuthorName.getText()+"'", null);
             if(c.moveToFirst())
             {
-                editName.setText(c.getString(1));
-                editMarks.setText(c.getString(2));
+                editAuthorEmailAddress.setText(c.getString(1));
+                editAuthorEmailMessage.setText(c.getString(2));
             }
             else
             {
-                showMessage("Error", "Invalid Rollno");
+                showMessage("Error", "Invalid auName");
                 clearText();
             }
         }
         if(view==btnViewAll)
         {
-            Cursor c=db.rawQuery("SELECT * FROM student", null);
+            Cursor c=db.rawQuery("SELECT * FROM delayEmail", null);
             if(c.getCount()==0)
             {
                 showMessage("Error", "No records found");
@@ -138,15 +114,15 @@ public class MainActivity extends Activity implements OnClickListener
             StringBuffer buffer=new StringBuffer();
             while(c.moveToNext())
             {
-                buffer.append("Rollno: "+c.getString(0)+"\n");
-                buffer.append("Name: "+c.getString(1)+"\n");
-                buffer.append("Marks: "+c.getString(2)+"\n\n");
+                buffer.append("auName: "+c.getString(0)+"\n");
+                buffer.append("auEmail: "+c.getString(1)+"\n");
+                buffer.append("auMsg: "+c.getString(2)+"\n\n");
             }
-            showMessage("Student Details", buffer.toString());
+            showMessage("delayEmail Details", buffer.toString());
         }
         if(view==btnShowInfo)
         {
-            showMessage("Student Management Application", "Developed By Azim");
+            showMessage("Devoter Time Machine", "Thanks to Azim");
         }
     }
     public void showMessage(String title,String message)
@@ -159,9 +135,9 @@ public class MainActivity extends Activity implements OnClickListener
     }
     public void clearText()
     {
-        editRollno.setText("");
-        editName.setText("");
-        editMarks.setText("");
-        editRollno.requestFocus();
+        editAuthorName.setText("");
+        editAuthorEmailAddress.setText("");
+        editAuthorEmailMessage.setText("");
+        editAuthorName.requestFocus();
     }
 }
